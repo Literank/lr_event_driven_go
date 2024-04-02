362c2619d55f5eacb6eebab258f3103544e4f4fd
netdong-li <east2050@gmail.com>
2024-04-02 14:03:23 +0800

    Initital version

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Create a new Gin router
+	router := gin.Default()
+
+	// Load HTML templates from the templates directory
+	router.LoadHTMLGlob("templates/*.html")
+
+	// Define a route for the homepage
+	router.GET("/", func(c *gin.Context) {
+		// Render the HTML template named "index.tmpl"
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "LiteRank Book Store",
+		})
+	})
+
+	// Run the server, default port is 8080
+	router.Run()
+}
